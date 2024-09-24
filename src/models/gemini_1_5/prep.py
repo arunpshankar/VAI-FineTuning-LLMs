e24afa4d95f72013615c822bb9293558704c3913
Arun Shankar <arunprshankar@gmail.com>
2024-09-24 11:20:58 -0500

    wip

--- src/models/gemini_1_5/prep.py
+++ src/models/gemini_1_5/prep.py
@@ -1,13 +1,16 @@
-import logging
+from src.config.logging import logger 
+from src.config.loader import config 
 from google.cloud import storage
-import os
-from typing import List, Dict
+from typing import Dict
+from typing import List 
 import jsonlines
-from src.config.config import config  # Import the Config singleton
+
 
 def prepare_data():
-    """Prepares the dataset for model tuning."""
-    logging.info("Starting data preparation.")
+    """
+    Prepares the dataset for model tuning.
+    """
+    logger.info("Starting data preparation.")
     try:
         # Access dataset paths from Config
         train_file_local = 'sft_train_samples.jsonl'
@@ -27,13 +30,16 @@ def prepare_data():
         upload_to_gcs(train_file_local, train_dataset_path)
         upload_to_gcs(val_file_local, val_dataset_path)
 
-        logging.info("Data preparation completed successfully.")
+        logger.info("Data preparation completed successfully.")
     except Exception as e:
-        logging.exception("An error occurred during data preparation.")
+        logger.exception("An error occurred during data preparation.")
         raise e
 
+
 def create_tuning_samples(file_path: str) -> List[Dict]:
-    """Creates tuning samples from a file."""
+    """
+    Creates tuning samples from a file.
+    """
     try:
         with jsonlines.open(file_path) as reader:
             instances = []
@@ -46,17 +52,21 @@ def create_tuning_samples(file_path: str) -> List[Dict]:
                 instances.append({"contents": instance})
         return instances
     except Exception as e:
-        logging.exception(f"Failed to create tuning samples from {file_path}.")
+        logger.exception(f"Failed to create tuning samples from {file_path}.")
         raise e
 
+
 def save_jsonlines(file: str, instances: List[Dict]) -> None:
-    """Saves a list of json instances to a jsonlines file."""
+    """
+    Saves a list of json instances to a jsonlines file.
+    """
     try:
         with jsonlines.open(file, mode="w") as writer:
             writer.write_all(instances)
     except Exception as e:
-        logging.exception(f"Failed to save jsonlines to {file}.")
+        logger.exception(f"Failed to save jsonlines to {file}.")
         raise e
+
 
 def upload_to_gcs(local_file: str, gcs_uri: str) -> None:
     """Uploads a local file to Google Cloud Storage."""
@@ -67,7 +77,7 @@ def upload_to_gcs(local_file: str, gcs_uri: str) -> None:
         bucket = client.bucket(bucket_name)
         blob = bucket.blob(destination_blob_name)
         blob.upload_from_filename(local_file)
-        logging.info(f"Uploaded {local_file} to {gcs_uri}.")
+        logger.info(f"Uploaded {local_file} to {gcs_uri}.")
     except Exception as e:
-        logging.exception(f"Failed to upload {local_file} to {gcs_uri}.")
+        logger.exception(f"Failed to upload {local_file} to {gcs_uri}.")
         raise e
